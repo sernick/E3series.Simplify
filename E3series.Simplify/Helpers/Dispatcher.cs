094ac7d95d77e7c631b95fc24f75dedd03f02248
Алексей Бурайкин <alex.buraykin@gmail.com>
2015-09-22 09:46:16 +0300

    Added Dispatcher for running E3.series applications

---
+++ E3series.Simplify/Helpers/Dispatcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using E3series.Simplify.Helpers.Native;
+
+namespace E3series.Simplify.Helpers
+{
+    static class Dispatcher
+    {
+        #region Public Methods
+
+        [STAThread]
+        public static IEnumerable<KeyValuePair<int, object>> GetE3Applications()
+        {
+            IRunningObjectTable runningObjectTable;
+            IEnumMoniker monikerEnumerator;
+            var monikers = new IMoniker[1];
+
+            IBindCtx ctx;
+            WinApi.CreateBindCtx(0, out ctx);
+
+            ctx.GetRunningObjectTable(out runningObjectTable);
+            runningObjectTable.EnumRunning(out monikerEnumerator);
+            monikerEnumerator.Reset();
+
+            while (monikerEnumerator.Next(1, monikers, IntPtr.Zero) == 0)
+            {
+                string runningObjectName;
+                monikers[0].GetDisplayName(ctx, null, out runningObjectName);
+
+                if (!runningObjectName.StartsWith("!E3Application", StringComparison.InvariantCultureIgnoreCase))
+                    continue;
+
+                int index = runningObjectName.IndexOf(':');
+                if (index == -1) continue;
+
+                object runningObjectVal;
+                runningObjectTable.GetObject(monikers[0], out runningObjectVal);
+
+                yield return
+                    new KeyValuePair<int, object>(int.Parse(runningObjectName.Substring(index + 1)),
+                        runningObjectVal);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ E3series.Simplify/Helpers/Native/WinApi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace E3series.Simplify.Helpers.Native
+{
+    static class WinApi
+    {
+        #region Dispatcher Use
+
+        [DllImport("ole32.dll")]
+        public static extern int CreateBindCtx(int reserved, out IBindCtx ppbc);
+
+        #endregion
+
+        #region Connector Use
+
+        public enum GetWindowCmd : uint
+        {
+            // ReSharper disable once InconsistentNaming
+            GW_HWNDNEXT = 2,
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindow(IntPtr hWnd, uint uCmd);
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr GetParent(IntPtr hWnd);
+
+        #endregion
+    }
+}
