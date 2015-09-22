3a1e64005d232a5c4989f1eef9e6f9fc5268694a
Алексей Бурайкин <alex.buraykin@gmail.com>
2015-09-22 09:56:58 +0300

    Now Connector may work with zero or one running E3.series

--- E3series.Simplify/Helpers/Connector.cs
+++ E3series.Simplify/Helpers/Connector.cs
@@ -1,18 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using e3;
+using E3series.Simplify.Helpers.Native;
 
 namespace E3series.Simplify.Helpers
 {
     public static class Connector
     {
+        #region Constants
+
+        private const string Identifier = "E3.series";
+
+        #endregion
+
+        #region Public Methods
+
+        [STAThread]
         public static e3Application Connect()
         {
-            throw new NotImplementedException();
+            var processList = Process.GetProcessesByName(Identifier);
+
+            switch (processList.Length)
+            {
+                case 0:
+                    return null;
+                case 1:
+                    return Connect(processList[0].Id);
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
+        [STAThread]
         public static e3Application Connect(int programId)
         {
-            throw new NotImplementedException();
+            if (programId != 0)
+            {
+                var keyValuePair = Dispatcher.GetE3Applications().FirstOrDefault(o => o.Key == programId);
+                if (!Equals(keyValuePair,default(KeyValuePair<int,object>)))
+                    return (e3Application) keyValuePair.Value;
+            }
+
+            return Connect();
         }
+
+        #endregion
     }
 }
