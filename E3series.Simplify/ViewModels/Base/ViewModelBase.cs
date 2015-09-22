1b3663283e1473122bc7564b125b4089194cdd6a
Алексей Бурайкин <alex.buraykin@gmail.com>
2015-09-22 09:50:20 +0300

    Added ViewModelBase and IView interface

---
+++ E3series.Simplify/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using E3series.Simplify.Views.Interfaces;
+
+namespace E3series.Simplify.ViewModels.Base
+{
+    class ViewModelBase<TViewType> : INotifyPropertyChanged
+    where TViewType : IView
+    {
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Private Fields
+
+        private readonly TViewType _view;
+
+        #endregion
+        
+        #region Public Fields
+
+        public TViewType View
+        {
+            get { return _view; }
+        }
+
+        #endregion
+        
+        #region Constructor
+
+        public ViewModelBase(TViewType view)
+        {
+            _view = view;
+            View.DataContext = this;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ E3series.Simplify/Views/Interfaces/IView.cs
@@ -0,0 +1,8 @@
+﻿namespace E3series.Simplify.Views.Interfaces
+{
+    interface IView
+    {
+        object DataContext { get; set; }
+        void Close();
+    }
+}
